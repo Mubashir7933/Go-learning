dd8770a0f2cc27d2fa40e80fe7c60455e14b0b27
Mubashir7933 <syed.mubashir.ahmed02@gmail.com>
2025-09-01 11:34:56 +0300

    Variable types

---
+++ 02_simple_values/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	// integer
+	fmt.Println(1 + 1)
+
+	// Variables
+	fmt.Println("Variables")
+}
---
+++ 03_variables/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// variables learning
+	var Middle string = "Mubashir"
+	fmt.Println(Middle) // normal variable declaration
+	first := "Syed"     // shorthand variable declaration
+	fmt.Println(first)
+	var name = "Ahmed" // type inference
+	fmt.Println(name)
+}
