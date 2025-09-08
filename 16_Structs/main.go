e626b8dca1def930764e640e6ad4d72d8317501b
Mubashir7933 <syed.mubashir.ahmed02@gmail.com>
2025-09-08 14:11:56 +0300

    Struct guide

--- 16_Structs/main.go
+++ 16_Structs/main.go
@@ -11,26 +11,56 @@ type Reservation struct {
 	Email        string
 	createdAt    time.Time
 	availability string
+	price        float64
 }
 
-func (res *Reservation) getStatus(availability string) {
-	res.availability = availability
-	fmt.Println(availability)
-}
+// constructor with pointer receiver
+
+// func newReservervation(id int, Name string, Email string, price float64) *Reservation {
+// 	centerPort := Reservation{
+// 		id:        id,
+// 		Name:      Name,
+// 		Email:     Email,
+// 		createdAt: time.Now(),
+// 		price:     price,
+// 	}
+// 	return &centerPort
+// }
+
+// func (res *Reservation) getStatus(availability string) {
+// 	res.availability = availability
+// 	fmt.Println(availability)
+// }
+
+// func (res Reservation) getPrice() float64 {
+// 	return res.price
+// }
 
 func main() {
 
-	centerPort := Reservation{
-		id:           1,
-		Name:         "Mubashir",
-		Email:        "mubashir@gmail.com",
-		createdAt:    time.Now(),
-		availability: "open",
-	}
+	// centerPort := Reservation{
+	// 	id:           1,
+	// 	Name:         "Mubashir",
+	// 	Email:        "mubashir@gmail.com",
+	// 	createdAt:    time.Now(),
+	// 	availability: "open",
+	// 	price:        500.00,
+	// }
 
-	centerPort.getStatus("closed")
-	fmt.Println(centerPort.availability)
+	// centerPort := newReservervation(1, "Mubashir", "mubashir030601@gmail.com", 500)
 
+	// centerPort.getStatus("closed")
+	// fmt.Println(centerPort.availability)
+	// fmt.Println(centerPort.getPrice())
+
+	//single use struct
+	// centerPort := struct {
+	// 	id    int
+	// 	name  string
+	// 	price float64
+	// }{1, "Mubashir", 500.00}
+
+	fmt.Println("Hello Structs")
 	// struct with pointer
 
 	// func ( res *Reservation ) getStatus(availability string)  {
