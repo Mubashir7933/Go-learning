4a42b334a7f22a9273fed75b0b52022b0840657a
Mubashir7933 <syed.mubashir.ahmed02@gmail.com>
2025-09-21 22:51:23 +0300

    Read file using byte

--- 24_Files/main.go
+++ 24_Files/main.go
@@ -1,19 +1,32 @@
 package main
 
-import (
-	"fmt"
-	"os"
-)
-
 func main() {
-	f, error := os.Open("textFile.txt")
-	if error != nil {
-		panic(error)
-	}
+	// f, error := os.Open("textFile.txt")
+	// if error != nil {
+	// 	panic(error)
+	// }
 
-	fileinfo, error := f.Stat()
-	if error != nil {
-		panic(error)
-	}
-	fmt.Println("Your file name is: ", fileinfo.Name())
+	// fileinfo, error := f.Stat()
+	// if error != nil {
+	// 	panic(error)
+	// }
+	// fmt.Println("Your file name is: ", fileinfo.Name())
+	// fmt.Println("Your file name is: ", fileinfo.Size())
+
+	//Reading file
+
+	// f, err := os.Open("textFile.txt")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer f.Close()
+
+	// buf := make([]byte, 1024)
+	// n, err := f.Read(buf)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// for i := 0; i < len(buf); i++ {
+	// 	println("Number of bytes read: ", n, string(buf[i]))
+	// }
 }
