1fe4d3c7fb8dbc7e33a59e8f59926e4564d6b03f
Mubashir7933 <syed.mubashir.ahmed02@gmail.com>
2025-09-21 23:13:42 +0300

    Reading folders

--- 24_Files/main.go
+++ 24_Files/main.go
@@ -29,4 +29,32 @@ func main() {
 	// for i := 0; i < len(buf); i++ {
 	// 	println("Number of bytes read: ", n, string(buf[i]))
 	// }
+
+	//Basic way to read file
+
+	// f, err := os.ReadFile("textFile.txt")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println(string(f))
+
+	//holds the whole data of the file in the memory using this way
+
+	//Reading folders
+
+	// dir, _err := os.Open(".")
+	// if _err != nil {
+	// 	panic(_err)
+	// }
+
+	// defer dir.Close()
+
+	// fileInfo, err := dir.Readdir(-1)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// for _, file := range fileInfo {
+	// 	fmt.Println(file.Name(), file.IsDir(), file.Size(), file.Mode())
+	// }
+
 }
