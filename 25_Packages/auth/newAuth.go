0611da45073e61f7ab2d8d721b417aa5d24d689e
Mubashir7933 <syed.mubashir.ahmed02@gmail.com>
2025-09-22 22:46:41 +0300

    Completion of concepts of Golang basics

--- 25_Packages/auth/newAuth.go
+++ 25_Packages/auth/newAuth.go
@@ -6,3 +6,7 @@ func LoginWithYourCredentials(username string, password string) {
 	fmt.Println("You are logged in with username:", username)
 	fmt.Println("\nYour password is:", password)
 }
+
+func Session() string {
+	return "sessionId"
+}
--- 25_Packages/main.go
+++ 25_Packages/main.go
@@ -1,8 +1,24 @@
 package main
 
-import "github.com/Mubashir7933/Go-learning/auth"
+import (
+	"fmt"
+
+	"github.com/Mubashir7933/Go-learning/auth"
+	"github.com/Mubashir7933/Go-learning/user"
+)
 
 func main() {
 	// Learning of how to work with packages in Go
 	auth.LoginWithYourCredentials("Mubashir", "mypassword123")
+	session := auth.Session()
+
+	println("Your session id is:", session)
+
+	UserObj := user.User{
+		Username: "Mubashir",
+		Email:    "mubashir@gmail.com",
+	}
+	fmt.Println("User details are:", UserObj)
+	fmt.Println("Username is:", UserObj.Username)
+	fmt.Println("Email is:", UserObj.Email)
 }
